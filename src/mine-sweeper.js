813d31c1f5f08b8c821fe620759339ccb76bfed0
Aliaksandr Naumavets <naum1979alex@gmail.com>
2023-12-22 01:12:57 +0300

    feat: complete email domain, mac address and mine sweeper tasks

--- src/mine-sweeper.js
+++ src/mine-sweeper.js
@@ -23,9 +23,49 @@ const { NotImplementedError } = require('../extensions/index.js');
  *  [1, 1, 1]
  * ]
  */
-function minesweeper(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function minesweeper(matrix) {
+  let res = [];
+  for (let i = 0; i < matrix.length; i += 1) {
+    let row = [];
+    for (let j = 0; j < matrix[i].length; j += 1) {
+      let minesCount = 0;
+      if ((i > 0) && (matrix[i - 1][j])) {
+        minesCount += 1;
+      }
+      if ((j > 0) && (matrix[i][j - 1])) {
+        minesCount += 1;
+      }
+      
+      if ((i > 0) && (j > 0) && (matrix[i - 1][j - 1])) {
+        minesCount += 1;
+      }
+
+      if ((i < matrix.length - 1) && (matrix[i + 1][j])) {
+        minesCount += 1;
+      }
+
+      if ((i > 1) && (j < matrix[i].length - 1) && (matrix[i - 1][j + 1])) {
+        minesCount += 1;
+      }
+
+      if ((i < matrix.length - 1) && 
+      (j < matrix[i].length - 1) && 
+      (matrix[i + 1][j + 1])) {
+        minesCount += 1;
+      }
+
+      if ((i < matrix.length - 1) && (j > 1) && (matrix[i + 1][j - 1])) {
+        minesCount += 1;
+      }
+
+      if ((j < matrix[i].length - 1) && matrix[i][j + 1]) {
+        minesCount += 1;
+      }
+      row.push(minesCount);
+    }
+    res.push(row);
+  }
+  return res;
 }
 
 module.exports = {
